f19bad5bdaf15017800579f86ff89e1d39ecf05e
luis fernando <fer.almeidabr@hotmail.com>
2021-12-07 22:42:41 -0300

    Component inicio sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { UserLogin } from '../model/UserLogin';
 import { Observable } from 'rxjs';
 import { User } from '../model/User';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,15 @@ export class AuthService {
 
   cadastrar(user: User): Observable<User> {
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+  }
+
+  logado(){
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 }
